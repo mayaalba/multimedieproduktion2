e35f6c4f975d18373515fb13a137899ae08aacac
annemoerk <48016188+annemoerk@users.noreply.github.com>
2019-05-21 15:13:53 +0200

    Add files via upload

--- lunde.js
+++ lunde.js
@@ -8,30 +8,39 @@ function myFunction() {
   }
 }
 
-/* KALENDER FORM */
-function on() {
-    document.getElementById("searchoverlay").style.display = "block";
+/* MODALBOLIG */
+// Get the modal
+var modal = document.getElementById("boligmodal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("filterbtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-function off() {
-    document.getElementById("searchoverlay").style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
-//search
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
-function searchFunction() {
-    let input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("searchInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("searchUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
+/* RANGESLIDER*/
+
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
 }
