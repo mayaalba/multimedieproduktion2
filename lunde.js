986385ff4fe4ecc3a12233a6d016282423f4a986
mayaalba <47300576+mayaalba@users.noreply.github.com>
2019-05-22 23:36:54 +0200

    Add files via upload

--- lunde.js
+++ lunde.js
@@ -8,73 +8,95 @@ function myFunction() {
   }
 }
 
-/* KALENDER */
-window.onload = function(){
-    let d = new Date();
-    let month_name = ['Januar','Februar','Marts','April','Maj','Juni','Juli','August','September','Oktober','November','December'];
-    let month = d.getMonth();   //0-11
-    let year = d.getFullYear(); //2014
-    let first_date = month_name[month] + " " + 1 + " " + year;
-    //September 1 2014
-    let tmp = new Date(first_date).toDateString();
-    //Mon Sep 01 2014 ...
-    let first_day = tmp.substring(0, 3);    //Mon
-    let day_name = ['Søn','Man','Tirs','Ons','Tors','Fre','Lør'];
-    let day_no = day_name.indexOf(first_day);   //1
-    let days = new Date(year, month+1, 0).getDate();    //30
-    //Tue Sep 30 2014 ...
-    let calendar = get_calendar(day_no, days);
-    document.getElementById("calendar-month-year").innerHTML = month_name[month]+" "+year;
-    document.getElementById("calendar-dates").appendChild(calendar);
+/* KALENDER FORM */
+function on() {
+    document.getElementById("searchoverlay").style.display = "block";
 }
 
-function get_calendar(day_no, days){
-    let table = document.createElement('table');
-    let tr = document.createElement('tr');
-    
-    //row for the day letters
-    for(let c=0; c<=6; c++){
-        let td = document.createElement('td');
-        td.innerHTML = "SMTOTFL"[c];
-        tr.appendChild(td);
-    }
-    table.appendChild(tr);
-    
-    //create 2nd row
-    tr = document.createElement('tr');
-    var c; //skal være var, ellers virker det ikke
-    for(c=0; c<=6; c++){
-        if(c == day_no){
-            break;
-        }
-        let td = document.createElement('td');
-        td.innerHTML = "";
-        tr.appendChild(td);
-    }
-    
-    let count = 1;
-    for(; c<=6; c++){
-        var td = document.createElement('td');
-        td.innerHTML = count;
-        count++;
-        tr.appendChild(td);
-    }
-    table.appendChild(tr);
-    
-    //rest of the date rows
-    for(let r=3; r<=7; r++){
-        tr = document.createElement('tr');
-        for(let c=0; c<=6; c++){
-            if(count > days){
-                table.appendChild(tr);
-                return table;
-            }
-            let td = document.createElement('td');
-            td.innerHTML = count;
-            count++;
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
-    }
-    return table;
+function off() {
+    document.getElementById("searchoverlay").style.display = "none";
 }
+
+//søgefunktion
+// definerer en funktion
+function searchFunction() {
+    //definerer variabler
+    let input, filter, ul, li, a, i, txtValue;
+    //giver variabler værdier fra html ved hjælp af operator
+    input = document.getElementById("searchInput");
+    //se event i html
+    filter = input.value.toUpperCase();
+    //filter=indtastet værdi i input
+    ul = document.getElementById("searchUL");
+    li = ul.getElementsByTagName("li");
+    // starter et loop
+    for (i = 0; i < li.length; i++) {
+        //med variablen a
+        a = li[i].getElementsByTagName("a")[0];
+        // giver variablen en værdi content eller inner
+        txtValue = a.textContent || a.innerText;
+        //hvis indt test passer vises matcher filter
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+            //hvis ikke vises intet
+        } else {
+            li[i].style.display = "none";
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//modal kalender
+// Get the modal
+let modal = document.getElementById("Modalkal");
+
+// Åben modal knap
+let btn = document.getElementById("myBtn");
+
+// span element der lukker modal
+let span = document.getElementsByClassName("close")[0];
+
+// Når man klikker på knappen åbner modalen 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Når der trygges på span elementet (x), lukker modalen
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Når der trykkes hvor som helst på siden lukkes modalen 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
